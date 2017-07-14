75b11714d79608e0b472f5a608029f73ae025349
Mariusz Ornowski <mariusz.ornowski@ict-project.pl>
2017-07-14 11:50:44 +0200

    json-object

--- source/json-object.cpp
+++ source/json-object.cpp
@@ -35,6 +35,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 **************************************************************/
 //============================================
 #include "json-object.hpp"
+#include "logger.hpp"
 #include <regex>
 //============================================
 #ifdef ENABLE_TESTING
@@ -480,6 +481,7 @@ int StringType::testJsonThis(){
   }
   if (jsonParams_regex){//Jeśli jest wyrażenie regularne.
     std::regex r(*jsonParams_regex);
+    LOGGER_DEBUG<<__LOGGER__<<"regex '"<<(*jsonParams_regex)<<"' on '"<<(*this)<<std::endl;
     if (!std::regex_match(*this,r)) {//Sprawdź wyrażenie regularne.
       error<<"Nieprawidłowy format (regex) elementu JSON String - powinien spełniać \""<<(*jsonParams_regex)<<"\"! ";
       e=true;
