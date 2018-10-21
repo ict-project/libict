f2103fb29a3c10023926225199f564e75ab4278a
Mariusz Ornowski <mariusz.ornowski@ict-project.pl>
2018-10-21 10:36:18 +0200

    data - array

---
+++ source/data-array.hpp
@@ -0,0 +1,91 @@
+//! @file
+//! @brief Data array module - Header file.
+//! @author Mariusz Ornowski (mariusz.ornowski@ict-project.pl)
+//! @version 1.0
+//! @date 2012-2018
+//! @copyright ICT-Project Mariusz Ornowski (ict-project.pl)
+/* **************************************************************
+Copyright (c) 2012-2018, ICT-Project Mariusz Ornowski (ict-project.pl)
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+1. Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+3. Neither the name of the ICT-Project Mariusz Ornowski nor the names
+of its contributors may be used to endorse or promote products
+derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**************************************************************/
+#ifndef _DATA_ARRAY_HEADER
+#define _DATA_ARRAY_HEADER
+//============================================
+#include <array>
+#include "data-interface.hpp"
+//============================================
+namespace ict { namespace data {
+//===========================================
+template<typename T, std::size_t N> 
+class array_array_t:public std::array<T,N>,public complex_interface {
+private:
+    typedef std::array<T,N> array_t;
+public:
+    //! Patrz: interface::data_clear()
+    void data_clear(const std::string & tag=""){
+        for(std::size_t k=0;k<N;k++) array_t::operator[](k).clear();
+    }
+    //! Patrz: interface::data_pushFront()
+    bool data_pushFront(const std::string & tag=""){
+        return(true);
+    } 
+    //! Patrz: interface::data_pushBack()
+    bool data_pushBack(const std::string & tag=""){
+        return(true);
+    }
+    //! Patrz: interface::data_getType()
+    data_t data_getType() const {
+        return(data_array_array);
+    }
+    //! Patrz: interface::data_getSize()
+    std::size_t data_getSize() const {
+        return(N);
+    }
+    //! Patrz: interface::data_getValue()
+    interface & data_getValue(const std::size_t & index) {
+        return(array_t::operator[](index));
+    }
+    const interface & data_getValue(const std::size_t & index) const {
+        return(array_t::at(index));
+    }
+    //! Patrz: interface::data_getFront()
+    virtual interface & data_getFront() {
+        return(array_t::front());
+    }
+    //! Patrz: interface::data_getBack()
+    virtual interface & data_getBack() {
+        return(array_t::back());
+    }
+};
+//===========================================
+template<typename T, std::size_t N>
+using array = array_array_t<T,N>;
+//===========================================
+} }
+//===========================================
+#endif
