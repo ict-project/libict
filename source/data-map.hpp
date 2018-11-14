8a5b2bdfcb16755c37c0ae12a788719ee7ebf22a
Mariusz Ornowski <mariusz.ornowski@ict-project.pl>
2018-11-14 16:08:23 +0100

    data mam - mistake

--- source/data-map.hpp
+++ source/data-map.hpp
@@ -41,7 +41,6 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <memory>
 #include "data-interface.hpp"
 //============================================
-std::map<int,int> t;
 namespace ict { namespace data {
 //===========================================
 #define CONSTRUCTORS(name,body)\
