05a71d98fde3b8a61cae3119073bd8adcd248e6f
Mariusz Ornowski <mariusz.ornowski@ict-project.pl>
2018-11-14 16:08:57 +0100

    cmake

--- source/all.hpp
+++ source/all.hpp
@@ -1,53 +1,30 @@
-//! @file
-//! @brief libict - Header file.
-//! @author Mariusz Ornowski (mariusz.ornowski@ict-project.pl)
-//! @version 1.0
-//! @date 2012-2017
-//! @copyright ICT-Project Mariusz Ornowski (ict-project.pl)
-/* **************************************************************
-Copyright (c) 2012-2017, ICT-Project Mariusz Ornowski (ict-project.pl)
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions
-are met:
-
-1. Redistributions of source code must retain the above copyright
-notice, this list of conditions and the following disclaimer.
-2. Redistributions in binary form must reproduce the above copyright
-notice, this list of conditions and the following disclaimer in the
-documentation and/or other materials provided with the distribution.
-3. Neither the name of the ICT-Project Mariusz Ornowski nor the names
-of its contributors may be used to endorse or promote products
-derived from this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-**************************************************************/
 #ifndef _LIBICT_HEADER
 #define _LIBICT_HEADER
-//============================================
 #include "base64.hpp"
-#include "utf8.hpp"
-#include "options.hpp"
-#include "time.hpp"
-#include "mutex.hpp"
-#include "os.hpp"
+#include "buffer.hpp"
+#include "daemon.hpp"
+#include "data-array.hpp"
+#include "data-deque.hpp"
+#include "data-forward_list.hpp"
+#include "data-info.hpp"
+#include "data-interface.hpp"
+#include "data-map.hpp"
+#include "data-object.hpp"
+#include "data-simple.hpp"
+#include "data-tuple.hpp"
+#include "data-types.hpp"
+#include "data-vector.hpp"
+#include "global.hpp"
+#include "json-object.hpp"
 #include "logger.hpp"
+#include "mutex.hpp"
+#include "options.hpp"
+#include "os.hpp"
+#include "pool.hpp"
 #include "random.hpp"
 #include "register.hpp"
-#include "daemon.hpp"
-#include "pool.hpp"
-#include "json-object.hpp"
-#include "data-interface.hpp"
-//===========================================
+#include "time.hpp"
+#include "utf8.hpp"
+#include "utf8-robot.hpp"
+#include "websocket.hpp"
 #endif
